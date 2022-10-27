5af5a1788684517909ba61f7e0442e6d66716361
Jamal raouj <jamalrwj@gmail.com>
2022-10-27 14:51:12 +0100

    I added persistence XML with postgresql

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,19 @@
+package Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class main {
+    public static void main(String[] args) {
+
+//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("digital_academy");
+//        System.out.println(entityManagerFactory.isOpen());
+//        System.exit(0);
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    }
+}
